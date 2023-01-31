14435a5b2517bcae9f9bc594f32f686cb86906a5
Berry, Eric <Eric_Berry@cable.comcast.com>
2023-01-31 10:05:45 -0800

    Issue 28: Fix integration issue with OpenSSL 1.1.1l

--- src/sec_adapter_engine.c
+++ src/sec_adapter_engine.c
@@ -28,8 +28,6 @@ static SEC_BOOL g_sec_openssl_inited = SEC_FALSE;
 static RSA_METHOD* rsa_method = NULL;
 #endif
 
-static ENGINE* engine = NULL;
-
 static void Sec_ShutdownOpenSSL() {
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
     if (rsa_method != NULL) {
@@ -38,10 +36,11 @@ static void Sec_ShutdownOpenSSL() {
     }
 #endif
 
+    ENGINE* engine = ENGINE_by_id(ENGINE_ID);
     if (engine != NULL) {
+        ENGINE_remove(engine);
         ENGINE_finish(engine);
         ENGINE_free(engine);
-        engine = NULL;
     }
 }
 
@@ -199,7 +198,7 @@ static RSA_METHOD g_sec_openssl_rsamethod = {
 #endif
 
 static void ENGINE_load_securityapi(void) {
-    engine = ENGINE_new();
+    ENGINE* engine = ENGINE_new();
     if (engine == NULL) {
         SEC_LOG_ERROR("ENGINE_new failed");
         return;
@@ -207,18 +206,15 @@ static void ENGINE_load_securityapi(void) {
 
     if (!ENGINE_set_id(engine, ENGINE_ID)) {
         ENGINE_free(engine);
-        engine = NULL;
         return;
     }
     if (!ENGINE_set_name(engine, "SecurityApi engine")) {
         ENGINE_free(engine);
-        engine = NULL;
         return;
     }
 
     if (!ENGINE_init(engine)) {
         ENGINE_free(engine);
-        engine = NULL;
         return;
     }
 
@@ -235,12 +231,13 @@ static void ENGINE_load_securityapi(void) {
 
     if (!ENGINE_set_RSA(engine, rsa_method)) {
 #endif
-        ENGINE_finish(engine);
+        ENGINE_remove(engine);
         ENGINE_free(engine);
-        engine = NULL;
         return;
     }
 
+    ENGINE_add(engine);
+    ENGINE_free(engine);
     ERR_clear_error();
 }
 
@@ -259,6 +256,9 @@ void Sec_InitOpenSSL() {
 
         ENGINE_load_builtin_engines();
         ENGINE_register_all_complete();
+        ENGINE_load_openssl();
+        ENGINE_set_default(ENGINE_by_id("openssl"), ENGINE_METHOD_ALL);
+        ENGINE_load_securityapi();
 
         if (atexit(Sec_ShutdownOpenSSL) != 0) {
             SEC_LOG_ERROR("atexit failed");
@@ -266,10 +266,6 @@ void Sec_InitOpenSSL() {
         }
 
         g_sec_openssl_inited = SEC_TRUE;
-    }
-
-    if (engine == NULL) {
-        ENGINE_load_securityapi();
     }
 
     pthread_mutex_unlock(&init_openssl_mutex);
@@ -283,19 +279,23 @@ void Sec_PrintOpenSSLVersion() {
 RSA* SecKey_ToEngineRSA(Sec_KeyHandle* keyHandle) {
     Sec_RSARawPublicKey pubKey;
     RSA* rsa = NULL;
+    ENGINE* engine = NULL;
 
+    engine = ENGINE_by_id(ENGINE_ID);
     if (engine == NULL) {
-        SEC_LOG_ERROR("engine not initialized");
+        SEC_LOG_ERROR("ENGINE_by_id failed");
         return NULL;
     }
 
     if (SEC_RESULT_SUCCESS != SecKey_ExtractRSAPublicKey(keyHandle, &pubKey)) {
+        ENGINE_free(engine);
         SEC_LOG_ERROR("SecKey_ExtractRSAPublicKey failed");
         return NULL;
     }
 
     rsa = RSA_new_method(engine);
     if (rsa == NULL) {
+        ENGINE_free(engine);
         SEC_LOG_ERROR("RSA_new_method failed");
         return NULL;
     }
@@ -309,25 +309,30 @@ RSA* SecKey_ToEngineRSA(Sec_KeyHandle* keyHandle) {
 #endif
 
     RSA_set_app_data(rsa, keyHandle);
+    ENGINE_free(engine);
     return rsa;
 }
 
 RSA* SecKey_ToEngineRSAWithCert(Sec_KeyHandle* keyHandle, Sec_CertificateHandle* certificateHandle) {
     Sec_RSARawPublicKey pubKey;
     RSA* rsa = NULL;
+    ENGINE* engine = NULL;
 
+    engine = ENGINE_by_id(ENGINE_ID);
     if (engine == NULL) {
         SEC_LOG_ERROR("ENGINE_by_id failed");
         return NULL;
     }
 
     if (SEC_RESULT_SUCCESS != SecCertificate_ExtractRSAPublicKey(certificateHandle, &pubKey)) {
+        ENGINE_free(engine);
         SEC_LOG_ERROR("SecKey_ExtractRSAPublicKey failed");
         return NULL;
     }
 
     rsa = RSA_new_method(engine);
     if (rsa == NULL) {
+        ENGINE_free(engine);
         SEC_LOG_ERROR("RSA_new_method failed");
         return NULL;
     }
@@ -341,6 +346,7 @@ RSA* SecKey_ToEngineRSAWithCert(Sec_KeyHandle* keyHandle, Sec_CertificateHandle*
 #endif
 
     RSA_set_app_data(rsa, keyHandle);
+    ENGINE_free(engine);
     return rsa;
 }
 
